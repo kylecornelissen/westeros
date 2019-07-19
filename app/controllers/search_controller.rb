e07ad9f8f6d161412740ef88f580363584125978
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-19 11:55:26 -0600

    mandatory commit 5

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     render locals: {
-      facade: HouseSearchFacade.new(params[:format])
+      facade: HouseSearchFacade.new(params[:house])
     }
   end
 end
--- spec/features/user/user_can_search_house_members.rb
+++ spec/features/user/user_can_search_house_members.rb
@@ -10,9 +10,9 @@ describe 'A registered user' do
     # And I click on "Get Members"
     click_on "Greyjoy"
     # Then my path should be "/search" with "house=greyjoy" in the parameters
-    expect(current_path).to eq('/search?house=greyjoy')
+    expect(current_path).to eq('/search')
     # expect(params).to eq("house=greyjoy")
-    expect(page).to have_current_path(people_path(house: 'greyjoy'))
+    expect(page).to have_current_path(search_path(house: 'greyjoy'))
     # And I should see a message "7 Members"
     expect(page).to have_content("7 Members")
     # And I should see a list of the 7 members of House Greyjoy
@@ -22,27 +22,27 @@ describe 'A registered user' do
           expect(page).to have_content('Balon Greyjoy')
           expect(page).to have_content('38')
         end
-        within('.member:nth-child(1)') do
+        within('.member:nth-child(2)') do
           expect(page).to have_content('Alannys Greyjoy')
           expect(page).to have_content('39')
         end
-        within('.member:nth-child(2)') do
+        within('.member:nth-child(3)') do
           expect(page).to have_content('Rodrik Greyjoy')
           expect(page).to have_content('40')
         end
-        within('.member:nth-child(3)') do
+        within('.member:nth-child(4)') do
           expect(page).to have_content('Maron Greyjoy')
           expect(page).to have_content('41')
         end
-        within('.member:nth-child(3)') do
+        within('.member:nth-child(5)') do
           expect(page).to have_content('Yara Greyjoy')
           expect(page).to have_content('42')
         end
-        within('.member:nth-child(3)') do
+        within('.member:nth-child(6)') do
           expect(page).to have_content('Theon Greyjoy')
           expect(page).to have_content('43')
         end
-        within('.member:nth-child(3)') do
+        within('.member:nth-child(7)') do
           expect(page).to have_content('Euron Greyjoy')
           expect(page).to have_content('44')
         end
